fcd84744edf0039c417531773cb6162578f5eb0f
dempseyb <dempsey.bullock@gmail.com>
2014-08-24 16:08:39 -0400

    Documentation changes
    Updated and removed remaining comments provided in the forked file
    rendition

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2 (Lexical Scoping)
 
 ## This function creates a special "matrix" object that can 
 ## cache its inverse.
